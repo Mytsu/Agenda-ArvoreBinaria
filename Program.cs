eccdbe7928ad4d8549dd6eb08acaf3ed4b213594
Jonathan Arantes <Mytsu@users.noreply.github.com>
2017-08-24 15:34:17 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace AgendaArvore
+{
+    class Program
+    {
+        static BinaryTree agenda = new BinaryTree();
+        static void Main(string[] args)
+        {
+            int leitor = 0;
+            while(leitor != 4) {
+                ExibirMenu();
+                if(Int32.TryParse(Console.ReadLine(), out leitor)) {
+                    switch (leitor)
+                    {
+                        case 1:
+                            AdicionarContato();
+                            break;
+
+                        case 2:
+                            PesquisarContato();
+                            break;
+                        
+                        case 3:
+                            RemoverContato();
+                            break;
+                    }
+                }                
+            }
+            
+        }
+
+        public static void ExibirMenu() {
+            Console.WriteLine("Agenda de Contatos: ");
+            Console.WriteLine("\t1. Adicionar Contato");
+            Console.WriteLine("\t2. Pesquisar Contato");
+            Console.WriteLine("\t3. Remover Contato");
+            Console.WriteLine("\t4. Sair");
+        }
+
+        public static void AdicionarContato() {
+            Console.WriteLine("Digite o nome do contato");
+            String nome = Console.ReadLine();
+            Console.WriteLine("Digite o telefone do contato");
+            String telefone = Console.ReadLine();
+            Console.WriteLine("Digite o endr do contato");
+            String endr = Console.ReadLine();
+            int id = GerarId(nome);
+            agenda.InsertNode(agenda.AddNode(id, nome, telefone, endr));
+        }
+
+        public static void PesquisarContato() {
+            Console.WriteLine("Digite o nome do contato");
+            String nome = Console.ReadLine();
+            int id = GerarId(nome);
+            Node contato = agenda.Pesquisa(id);
+            if(contato != null) {
+                Console.WriteLine("Dados do contato");
+                Console.WriteLine("Nome: " + contato.getNome());
+                Console.WriteLine("Telefone: " + contato.getTelefone());
+                Console.WriteLine("Endereço: " + contato.getEndr());
+            } else Console.WriteLine("Contato não encontrado.");
+        }
+
+        public static void RemoverContato() {
+            Console.WriteLine("Digite o nome do contato");
+            String nome = Console.ReadLine();
+            int id = GerarId(nome);
+            agenda.Remover(id);
+        }
+
+        public static int GerarId(String nome) {
+            int id = 0;
+            int i = 1;
+            foreach(char c in nome) {
+                id += Convert.ToInt32(c) *i;
+                ++i;
+            }
+            return id;
+        }
+    }
+}
---
+++ Tree.cs
@@ -0,0 +1,193 @@
+using System;
+
+namespace AgendaArvore {
+    class Node {
+        private int id;
+        private String nome;
+        private String telefone;
+        private String endr;
+        private Node left, right;
+
+        public Node(int id, String nome, String telefone, String endr) {
+            this.id = id;
+            this.nome = nome;
+            this.telefone = telefone;
+            this.endr = endr;
+            left = null;
+            right = null;
+        }
+
+        public int getId() {
+            return this.id;
+        }
+
+        public String getNome() {
+            return this.nome;
+        }
+
+        public String getTelefone() {
+            return this.telefone;
+        }
+
+        public String getEndr() {
+            return this.endr;
+        }
+
+        public Node getLeft() {
+            return this.left;
+        }
+
+        public Node getRight() {
+            return this.right;
+        }
+
+        public void setId(int id) {
+            this.id = id;
+        }
+
+        public void setNome(String nome) {
+            this.nome = nome;
+        }
+
+        public void setTelefone(String telefone) {
+            this.telefone = telefone;
+        }
+
+        public void setEndr(String endr) {
+            this.endr = endr;
+        }
+
+        public void setLeft(Node root) {
+            this.left = root;
+        }
+
+        public void setRight(Node root) {
+            this.right = root;
+        }
+    }
+    class BinaryTree {
+        public Node root;
+        static int count = 0;
+
+        public BinaryTree() {
+            root = null;
+        }
+
+        public BinaryTree(int id, String nome, String telefone, String endr) {
+            root = new Node(id, nome, telefone, endr);
+        }
+
+        public Node AddNode(int id, String nome, String telefone, String endr) {
+            return new Node(id, nome, telefone, endr);
+        }
+
+        public void InsertNode(Node newNode) {
+            Node atual = this.root, pai = null;
+            while(atual != null)
+            {
+                if(atual.getId() == newNode.getId())
+                {
+                    // dados iguais, ignorar
+                    return;
+                }
+                else if(atual.getId() > newNode.getId())
+                {
+                    // valor atual > entrada, adicionar a esquerda
+                    pai = atual;
+                    atual = atual.getLeft();
+                }
+                else if(atual.getId() < newNode.getId())
+                {
+                    // valor atual < entrada, adicionar a direita
+                    pai = atual;
+                    atual = atual.getRight();
+                }
+            }
+            count++;
+            if(pai == null)
+            {
+                // arvore vazia, fazer deste a raiz
+                root = newNode;
+            }
+            else
+            {
+                if (pai.getId() > newNode.getId())
+                {
+                    pai.setLeft(newNode);
+                }
+                else
+                    pai.setRight(newNode);
+            }
+        }
+
+        public Node Pesquisa(int data) {
+            return this.Pesquisa(this.root, data);
+        }
+
+        public Node Pesquisa(Node root, int data) {
+            if(root == null) return null;
+            if (root.getId() == data) return root;            
+            else if (root.getId() > data && root.getLeft() != null) {
+                return Pesquisa(root.getLeft(), data);
+            }
+            else if (root.getId() < data && root.getRight() != null) {
+                return Pesquisa(root.getRight(), data);
+            } else return null;
+        }
+
+        public void Remover(int data) {
+            Remover(this.root, data);
+        }
+
+        public Node Remover(Node root, int data) {
+            // funcao Remover recursiva
+            // caso base, arvore vazia
+            if(root == null) return root;
+            // recursao, descendo a arvore
+            if(root.getId() < data) {
+                root.setLeft(Remover(root.getLeft(), data));
+            } 
+            else if(root.getId() > data) {
+                root.setRight(Remover(root.getRight(), data));
+            }
+            // chave (data) e a mesma da raiz atual
+            // deletando
+            else {
+                // node com apenas um filho ou nenhum
+                if (root.getLeft() == null) {
+                    return root.getRight();
+                }
+                else if (root.getRight() == null) {
+                    return root.getLeft();
+                }
+
+                // node com 2 filhos, pegar o sucessor em ordem (o menor)
+                root.setId(MenorValor(root.getLeft()));
+                // deletar sucessor em ordem
+
+            }
+        }
+
+        public int MenorValor(Node root) {
+            int min = root.getId();
+            while (root.getLeft() != null) {
+                min = root.getLeft().getId();
+                root = root.getLeft();
+            }
+            return min;
+        }
+
+        public int Altura(Node root) {
+            if (root == null || root.getLeft() == null || root == null) return 0;
+            else {
+                return 1 + Maior(a: Altura(root.getLeft()), b: Altura(root.getRight()));
+            }
+        }
+
+        public int Maior(int a, int b) {
+            if(a > b) {
+                return a;
+            } else return b;
+        }
+    }
+}
